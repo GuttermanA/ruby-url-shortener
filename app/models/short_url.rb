aa6a27534591181b786c7c4598ba784a58594035
GuttermanA <alexanderf.gutterman@gmail.com>
2021-05-26 23:33:22 -0400

    add validation for unique full_url

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,7 +1,7 @@
 require 'uri'
 class ShortUrl < ApplicationRecord
     CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
-
+    validates :full_url, uniqueness: true, presence: true
     validate :validate_full_url
 
     def short_code
