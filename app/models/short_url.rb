113ba8818f411cdbb97add773d382e8d80a503bd
GuttermanA <alexanderf.gutterman@gmail.com>
2021-05-26 23:24:01 -0400

    remove print

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -16,7 +16,6 @@ class ShortUrl < ApplicationRecord
   def validate_full_url
     uri = URI.parse(full_url)
     unless uri.is_a?(URI::HTTP) && !uri.host.nil?
-        print 'URI ERROR'
         errors.add(:full_url, "Full url is not a valid url")
     end
   end
