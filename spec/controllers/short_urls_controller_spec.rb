676dd51483c99b894c9fc9a64e3285e0bbfaa29f
GuttermanA <alexanderf.gutterman@gmail.com>
2021-05-28 00:41:56 -0400

    code formatting

--- spec/controllers/short_urls_controller_spec.rb
+++ spec/controllers/short_urls_controller_spec.rb
@@ -1,11 +1,9 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe ShortUrlsController, type: :controller do
-
   let(:parsed_response) { JSON.parse(response.body) }
 
   describe "index" do
-
     let!(:short_url) { ShortUrl.create(full_url: "https://www.test.rspec") }
 
     it "is a successful response" do
@@ -16,27 +14,23 @@ RSpec.describe ShortUrlsController, type: :controller do
     it "has a list of the top 100 urls" do
       get :index, format: :json
 
-      expect(parsed_response['urls']).to be_include(short_url.public_attributes)
+      expect(parsed_response["urls"]).to be_include(short_url.public_attributes)
     end
-
   end
 
   describe "create" do
-
     it "creates a short_url" do
       post :create, params: { full_url: "https://www.test.rspec" }, format: :json
-      expect(parsed_response['short_code']).to be_a(String)
+      expect(parsed_response["short_code"]).to be_a(String)
     end
 
     it "does not create a short_url" do
       post :create, params: { full_url: "nope!" }, format: :json
-      expect(parsed_response['errors']).to be_include("Full url is not a valid url")
+      expect(parsed_response["errors"]).to be_include("Full url is not a valid url")
     end
-
   end
 
   describe "show" do
-
     let!(:short_url) { ShortUrl.create(full_url: "https://www.test.rspec") }
 
     it "redirects to the full_url" do
@@ -52,7 +46,5 @@ RSpec.describe ShortUrlsController, type: :controller do
     it "increments the click_count for the url" do
       expect { get :show, params: { id: short_url.short_code }, format: :json }.to change { ShortUrl.find(short_url.id).click_count }.by(1)
     end
-
   end
-
 end
