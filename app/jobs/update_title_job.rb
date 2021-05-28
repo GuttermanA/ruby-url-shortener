c41601468e4063b25e7af42185fb15712e1a8947
GuttermanA <alexanderf.gutterman@gmail.com>
2021-05-27 23:05:31 -0400

    remove console statement

--- app/jobs/update_title_job.rb
+++ app/jobs/update_title_job.rb
@@ -4,7 +4,6 @@ class UpdateTitleJob < ApplicationJob
   queue_as :default
 
   def perform(short_url_id)
-    puts short_url_id
     @short_url = ShortUrl.find_by(id: short_url_id)
     doc = Nokogiri::HTML(URI.open(@short_url.full_url))
     @short_url.title = doc.at_css("title").text
