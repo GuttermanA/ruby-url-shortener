c9119cacc0cac8ead5523ce0ea5d86673aec646c
GuttermanA <alexanderf.gutterman@gmail.com>
2021-05-27 22:00:29 -0400

    add uniqueness options to surpress rails warnings

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -2,7 +2,7 @@ require "uri"
 
 class ShortUrl < ApplicationRecord
   CHARACTERS = [*"0".."9", *"a".."z", *"A".."Z"].freeze
-  validates :full_url, uniqueness: true, presence: true
+  validates :full_url, uniqueness: { case_sensitive: false }, presence: true
   validate :validate_full_url
 
   def short_code
