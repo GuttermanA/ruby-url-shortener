98f821c92149b0baa96c3c190ded1741fc2a86f8
GuttermanA <alexanderf.gutterman@gmail.com>
2021-05-27 21:27:36 -0400

    code formatter

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,22 +1,22 @@
-require 'uri'
+require "uri"
+
 class ShortUrl < ApplicationRecord
-    CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
-    validates :full_url, uniqueness: true, presence: true
-    validate :validate_full_url
+  CHARACTERS = [*"0".."9", *"a".."z", *"A".."Z"].freeze
+  validates :full_url, uniqueness: true, presence: true
+  validate :validate_full_url
 
-    def short_code
+  def short_code
+  end
+
+  def update_title!
+  end
+
+  private
+
+  def validate_full_url
+    uri = URI.parse(full_url)
+    unless uri.is_a?(URI::HTTP) && !uri.host.nil?
+      errors.add(:full_url, "Full url is not a valid url")
     end
-
-    def update_title!
-    end
-
-    private
-
-    def validate_full_url
-        uri = URI.parse(full_url)
-        unless uri.is_a?(URI::HTTP) && !uri.host.nil?
-            errors.add(:full_url, "Full url is not a valid url")
-        end
-    end
-
+  end
 end
