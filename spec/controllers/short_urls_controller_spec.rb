1f8b97be3c70af54beb51585d4da2c28282ba1e7
GuttermanA <alexanderf.gutterman@gmail.com>
2021-06-01 21:15:55 -0400

    add status code expects to controller tests without them

--- spec/controllers/short_urls_controller_spec.rb
+++ spec/controllers/short_urls_controller_spec.rb
@@ -13,7 +13,6 @@ RSpec.describe ShortUrlsController, type: :controller do
 
     it "has a list of the top 100 urls" do
       get :index, format: :json
-
       expect(parsed_response["urls"]).to be_include(short_url.public_attributes)
     end
   end
@@ -35,6 +34,7 @@ RSpec.describe ShortUrlsController, type: :controller do
 
     it "does not create a short_url" do
       post :create, params: { full_url: "nope!" }, format: :json
+      expect(response.status).to eq 422
       expect(parsed_response["errors"]).to be_include("Full url is not a valid url")
     end
   end
@@ -44,6 +44,7 @@ RSpec.describe ShortUrlsController, type: :controller do
 
     it "redirects to the full_url" do
       get :show, params: { id: short_url.short_code }, format: :json
+      expect(response.status).to eq 302
       expect(response).to redirect_to(short_url.full_url)
     end
 
